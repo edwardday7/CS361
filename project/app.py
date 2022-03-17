88371960a941ab57365a13f7243f294351253236
Edward <eday@crimson.ua.edu>
2022-03-16 23:50:27 -0400

    Update project

--- project/app.py
+++ project/app.py
@@ -14,7 +14,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = '127.0.0.1'
+SERVER = '10.0.0.21'
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
